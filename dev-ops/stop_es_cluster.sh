e30e83333f258fc0ccef00405c06ae42af5b34f4
Andrew Anderson <anderson@elastic.co>
2018-11-20 22:24:46 -0700

    Initial commit

---
+++ dev-ops/stop_es_cluster.sh
@@ -0,0 +1,6 @@
+kill `cat /var/run/elasticsearch/pid9200`
+kill `cat /var/run/elasticsearch/pid9201`
+kill `cat /var/run/elasticsearch/pid9202`
+kill `cat /var/run/elasticsearch/pid9203`
+kill `cat /var/run/elasticsearch/pid9204`
+kill `cat /var/run/elasticsearch/pid9205`
