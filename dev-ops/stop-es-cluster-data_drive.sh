efa54d4451c4e07b2b8cb55ade132bdfad4c657f
Andrew Anderson <anderson@elastic.co>
2019-01-02 10:46:47 -0500

    Attempts at start script versioning, cleanup

--- dev-ops/stop-es-cluster-data_drive.sh
+++ dev-ops/stop-es-cluster-data_drive.sh
@@ -3,9 +3,9 @@ cd
 cd workspace/tesla-stack/dev-ops/
 
 #./routing_allocation-disable.sh
-kill `cat /var/run/elasticsearch/pid-data_drive-9200`
-kill `cat /var/run/elasticsearch/pid-data_drive-9201`
-kill `cat /var/run/elasticsearch/pid-data_drive-9202`
-kill `cat /var/run/elasticsearch/pid-data_drive-9203`
-kill `cat /var/run/elasticsearch/pid-data_drive-9204`
-kill `cat /var/run/elasticsearch/pid-data_drive-9205`
+kill `cat /data/data_drive/pid/9200`
+kill `cat /data/data_drive/pid/9201`
+kill `cat /data/data_drive/pid/9202`
+kill `cat /data/data_drive/pid/9203`
+kill `cat /data/data_drive/pid/9204`
+kill `cat /data/data_drive/pid/9205`
--- dev-ops/stop-es-cluster.sh
+++ dev-ops/stop-es-cluster.sh
@@ -9,3 +9,7 @@ kill `cat /var/run/elasticsearch/pid9202`
 kill `cat /var/run/elasticsearch/pid9203`
 kill `cat /var/run/elasticsearch/pid9204`
 kill `cat /var/run/elasticsearch/pid9205`
+
+kill `cat /data/elasticsearch/cluster.plane-12-29-2018/pid/9200`
+kill `cat /data/elasticsearch/cluster.plane-12-29-2018/pid/9201`
+kill `cat /data/elasticsearch/cluster.plane-12-29-2018/pid/9202`
