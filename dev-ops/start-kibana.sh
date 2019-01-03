cbf9830fedd9ef5b6b9c3b60a1a5cbc26e9c1e0b
Andrew Anderson <anderson@elastic.co>
2019-01-02 23:27:55 -0500

    Fix start/stop kibana scripts, pid files

--- dev-ops/start-kibana.sh
+++ dev-ops/start-kibana.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 dir=`pwd`
-/Users/andrew/workspace/elastic-stack/current/kibana/bin/kibana>> ~/workspace/tesla-stack/logs/kibana/kibana.out &
+#/Users/andrew/workspace/elastic-stack/current/kibana/bin/kibana>> ~/workspace/tesla-stack/logs/kibana/kibana.out &
 
 cd
 cd workspace/elastic-stack/current/kibana
-./bin/kibana  >> ~/workspace/tesla-stack/logs/kibana/kibana.out & echo $! > /var/run/kibana/kibana.pid
-echo "Kibana started with pid file `cat /var/run/kibana/kibana.pid`"
+./bin/kibana  >> ~/workspace/tesla-stack/logs/kibana/kibana.out & echo $! > kibana.pid
+echo "Kibana started with pid `cat kibana.pid`"
 
 cd $dir
