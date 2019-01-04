22aae37cd04f21d878da68620ce12e094fefd396
Andrew Anderson <anderson@elastic.co>
2019-01-04 16:20:40 -0500

    pskibana improvements, stop kibana and kibana2, with file checks

--- dev-ops/pskibana.sh
+++ dev-ops/pskibana.sh
@@ -1,6 +1,21 @@
 cd
 cd ~/workspace/elastic-stack/current/kibana/
 echo "grepping for kibana.pid files.."
-ps -ef | grep cat `kibana.pid`
-#ps -ef | grep cat `kibana2.pid`
 
+#ps -ef | grep cat "`kibana.pid`"
+#ps -ef | grep cat "`kibana2.pid`"
+KIBANA1=kibana.pid
+KIBANA2=kibana2.pid
+#./routing_allocation-disable.sh
+if [ ! -f $KIBANA1 ]; then
+  echo "No pid file found: `pwd`/$KIBANA1"
+fi
+if [ ! -f $KIBANA2 ]; then
+  echo "No pid file found: `pwd`/$KIBANA2"
+fi
+if [ -f $KIBANA1 ]; then
+  ps -ef | grep cat "`kibana.pid`"
+fi
+if [ -f $KIBANA2 ]; then
+  ps -ef | grep cat "`kibana2.pid`"
+fi
