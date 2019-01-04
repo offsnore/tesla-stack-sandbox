5f24c3877922276a6bcafb037a01829cfddc990c
Andrew Anderson <anderson@elastic.co>
2019-01-04 16:04:19 -0500

    ps kibana scripts, stop kibana1,2 scripts

--- dev-ops/kill-elasticsearch-pids-v2-data_drive.sh
+++ dev-ops/kill-elasticsearch-pids-v2-data_drive.sh
@@ -1,6 +1,7 @@
 cd /data/elasticsearch
 CLUSTER_NAME=data-drive.2019-01-02
 DATA_DIR=/data/data_drive/elasticsearch
-for f in ${DATA_DIR}/${CLUSTER_NAME}/pids/*; do kill `cat $f` && echo "killed Elasticsearch at pid $f"; done
+for f in ${DATA_DIR}/${CLUSTER_NAME}/pids/*.pid; do kill `cat $f` && echo "killed Elasticsearch at pid $f"; done
+for f in ${DATA_DIR}/${CLUSTER_NAME}/pids/*.pid; do rm $f && echo "removed pid file $f.."; done
 
 
