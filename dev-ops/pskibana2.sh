8bb51514327f683ab6a417233188d6a518d4143c
Andrew Anderson <anderson@elastic.co>
2019-01-04 16:04:19 -0500

    ps kibana scripts, stop kibana1,2 scripts

---
+++ dev-ops/pskibana2.sh
@@ -0,0 +1,7 @@
+cd
+#cd ~/workspace/elastic-stack/current/kibana
+echo "grepping for kibana.pid file.."
+#ps -ef | grep `cat kibana2.pid`
+
+cd ~/workspace/tesla-stack/dev-ops
+./pskibana2.sh
--- dev-ops/stop-kibana.sh
+++ dev-ops/stop-kibana.sh
@@ -4,4 +4,5 @@ cd workspace/elastic-stack/current/kibana
 
 #./routing_allocation-disable.sh
 echo "Klling kibana at pid `cat kibana.pid`.." && kill `cat kibana.pid`
-rm kibana.pid
+echo "Klling kibana2 at pid `cat kibana2.pid`.." && kill `cat kibana2.pid`
+rm kibana.pid kibana2.pid
