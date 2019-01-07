2ffc7adf512d533d046b1e74fd3d7e60bfa0f4cc
Andrew Anderson <anderson@elastic.co>
2019-01-07 12:10:29 -1000

    Default configs restructure, create/coopy default configs as part of startup/init scripts

---
+++ dev-ops/test-cluster-configs-exist.sh
@@ -0,0 +1,15 @@
+echo "Checking that the directories and config files exist for nodes.."
+# TODO: directory and file level checking
+cd 
+cd /data/local_drive/elasticsearch/
+alias ll='ls -al'
+ll
+ll local_data.2019-01-07
+ll local_data.2019-01-07/configs
+ll local_data.2019-01-07/configs/node-d0
+ll local_data.2019-01-07/configs/node-d1
+ll local_data.2019-01-07/configs/node-d2
+ll local_data.2019-01-07/data
+ll local_data.2019-01-07/logs
+ll local_data.2019-01-07/pids
+
