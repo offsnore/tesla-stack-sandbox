7f299e7498c1ff3f5b8de92d4e8edc4b3081000b
Andrew Anderson <anderson@elastic.co>
2019-01-08 13:46:57 -1000

    Auto create directories, default configs based on start script

--- dev-ops/start-es-cluster-v6-data_drive.sh
+++ dev-ops/start-es-cluster-v6-data_drive.sh
@@ -4,14 +4,14 @@ cd
 STACK_DIR=~/workspace/elastic-stack/current
 PROJECT_STACK=~/workspace/tesla-stack
 ES_HOME=$STACK_DIR/elasticsearch
-CLUSTER_NAME=local_data.2019-01-07
+CLUSTER_NAME=data_drive.2019-01-08
 
-if [ -z $ENV_CLUSTER_NAME ] ; then
-  echo "Setting cluster.name with env vare: $ENV_CLUSTER_NAME"
-  CLUSTER_NAME=$ENV_CLUSTER_NAME
-fi
+#if [ -z $ENV_CLUSTER_NAME ] ; then
+#  echo "Setting cluster.name with env vare: $ENV_CLUSTER_NAME"
+#  CLUSTER_NAME=$ENV_CLUSTER_NAME
+#fi
 
-DATA_DRIVE=/data/local_drive
+DATA_DRIVE=/data/data_drive
 CLUSTER_DIR=$DATA_DRIVE/elasticsearch/$CLUSTER_NAME
 
 DATA_DIR=$CLUSTER_DIR/data
@@ -60,7 +60,7 @@ fi
 if [ ! -z $PATH_CONFIGS ]; then
 # Need root privs to create data, config, log paths
   sudo mkdir -p $PATH_CONFIGS
-  echo "Created pids directory: $PATH_CONFIGS"
+  echo "Created configs path directory: $PATH_CONFIGS"
 fi
 
 
@@ -71,7 +71,7 @@ else cd ~/workspace/elastic-stack/current/elasticsearch
 fi
 
 # NODE 1
-NODE_NAME=node-0
+NODE_NAME=node-d0
 NODE_HEAP_GB=4
 NODE_TYPE=$HOT_NODE_TYPE
 if [ ! -z $DATA_DIR/$NODE_NAME ]; then
@@ -98,15 +98,16 @@ sudo chown -R andrew:wheel ${LOGS_DIR}
 sudo chown -R andrew:wheel ${PIDS_DIR}
 
 
+
 ES_JAVA_OPTS="-Xms4g -Xmx4g" ES_PATH_CONF="${PATH_CONFIGS}/${NODE_NAME}" ./bin/elasticsearch -p ${PIDS_DIR}/${NODE_NAME}.pid -E path.data=${DATA_DIR}/${NODE_NAME}  -E path.logs=${LOGS_DIR}/$NODE_NAME  -E "cluster.name=${CLUSTER_NAME}" -E node.name=${NODE_NAME}  -d
 echo "Started Elasticsearch $NODE_NAME"
 echo "Logs are at $LOGS_DIR"
 echo "Data is at $DATA_DIR"
-echo "Configs created are ate $PATHS_CONFIG/$NODE_NAME"
+echo "Configs created are at $PATHS_CONFIG/$NODE_NAME"
 
 # NODE 2
 
-NODE_NAME=node-1
+NODE_NAME=node-d1
 NODE_HEAP_GB=2
 NODE_TYPE=$COOL_NODE_TYPE
 if [ ! -z $DATA_DIR/$NODE_NAME ]; then
@@ -142,7 +143,7 @@ echo "Configs created are ate $PATHS_CONFIG/$NODE_NAME"
 
 # NODE 3
 
-NODE_NAME=node-2
+NODE_NAME=node-d2
 NODE_HEAP_GB=2
 NODE_TYPE=$COOL_NODE_TYPE
 if [ ! -z $DATA_DIR/$NODE_NAME ]; then
@@ -155,10 +156,10 @@ if [ ! -z $PATH_CONFIGS/$NODE_NAME  ]; then
   echo "Created node config directory: $PATH_CONFIGS/$NODE_NAME"
   echo "Copying default configs from: $DEFAULT_CONFIGS_PATH_ES/$NODE_TYPE"
   if [ ! -f $PATH_CONFIGS/$NODE_NAME/elasticsearch.yml ]; then 
-    sudo cp -rf $DEFAULT_CONFIGS_PATH_ES/$NODE_TYPE $PATH_CONFIGS/$NODE_NAME
+    sudo cp -rf $DEFAULT_CONFIGS_PATH_ES/$NODE_TYPE/* $PATH_CONFIGS/$NODE_NAME
   fi 
 fi
-if [ -z ES_HOME ]; then
+if [ -z ES_HOME ]; then 
   cd $ES_HOME
 else cd ~/workspace/elastic-stack/current/elasticsearch
 fi
@@ -175,4 +176,4 @@ echo "Logs are at $LOGS_DIR"
 echo "Data is at $DATA_DIR"
 echo "Configs created are ate $PATHS_CONFIG/$NODE_NAME"
 echo "Creating alias to tail all elasticsearch logs: tail-elasticsaerch-logs"
-alias tail-elasticsearch-logs="tail -F node-d*/data_drive.2019-01-08.log"
+alias tail-elasticsearch-logs="tail -F $DATA_DIR/*/data_drive.2019-01-08.log"
