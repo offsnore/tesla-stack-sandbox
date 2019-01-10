0d0d9bde97aecaafaa67084f52d7f7d54f760bd3
Andrew Anderson <anderson@elastic.co>
2019-01-09 18:28:27 -1000

    Resolve merge conflict

--- api-ops/api-gui-settings.sh
+++ api-ops/api-gui-settings.sh
@@ -1,5 +1,5 @@
 curl  "https://owner-api.teslamotors.com/api/1/vehicles/${VEHICLE_ID}/data_request/gui_settings" \
-     -H 'client_id: "${TESLA_API_CLIENT_ID}"' \
-     -H 'client_secret: "${TESLA_API_CLIENT_SECRET}"' \
+     -H 'client_id: 81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384' \
+     -H 'client_secret: c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3' \
      -H 'Authorization: Bearer ${TESLA_API_TOKEN}' \
      -o "gui_settings-response-$(date +%H:%M.%s).json"
--- api-ops/api-vehicle-state.sh
+++ api-ops/api-vehicle-state.sh
@@ -1,10 +1,6 @@
 ## Vehicle State
-<<<<<<< HEAD
 curl "https://owner-api.teslamotors.com/api/1/vehicles/${VEHICLE_ID}/data_request/vehicle_state" \
-=======
-curl "https://owner-api.teslamotors.com/api/1/vehicles/"${VEHICLE_ID}" /data_request/vehicle_state" \
->>>>>>> 68b783ba84de627248a7c0eada88368868881036
-     -H 'client_id: "${TESLA_API_CLIENT_ID}"' \
-     -H 'client_secret: "${TESLA_API_CLIENT_SECRET}"' \
+     -H 'client_id: 81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384' \
+     -H 'client_secret: c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3' \
      -H 'Authorization: Bearer ${TESLA_API_TOKEN}' \
      -o "vehicle_state-response-$(date +%H:%M.%s).json"
