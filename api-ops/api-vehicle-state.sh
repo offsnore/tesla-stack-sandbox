0086e5956db882e48f150f0bd952c39e2a90cea0
Andrew Anderson <anderson@elastic.co>
2019-01-09 18:28:27 -1000

    Resolve merge conflict

--- api-ops/api-vehicle-state.sh
+++ api-ops/api-vehicle-state.sh
@@ -1,9 +1,5 @@
 ## Vehicle State
-<<<<<<< HEAD
 curl "https://owner-api.teslamotors.com/api/1/vehicles/${VEHICLE_ID}/data_request/vehicle_state" \
-=======
-curl "https://owner-api.teslamotors.com/api/1/vehicles/"${VEHICLE_ID}" /data_request/vehicle_state" \
->>>>>>> 68b783ba84de627248a7c0eada88368868881036
      -H 'client_id: "${TESLA_API_CLIENT_ID}"' \
      -H 'client_secret: "${TESLA_API_CLIENT_SECRET}"' \
      -H 'Authorization: Bearer ${TESLA_API_TOKEN}' \
